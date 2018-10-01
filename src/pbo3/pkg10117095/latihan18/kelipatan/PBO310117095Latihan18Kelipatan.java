508ee430048f7f199023351a0d1151725051f6ce
alvinlukmanNH <galinx20gmail.com>
2018-10-01 20:06:51 +0700

    Kelipatan

---
+++ src/pbo3/pkg10117095/latihan18/kelipatan/PBO310117095Latihan18Kelipatan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117095.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * Nama      : Alvin Lukman Nulhakim 
+ * Kelas     : IF3 
+ * NIM       : 10117095 
+ * Deskripsi : Program ini berisi program untuk menghitung kelipatan dari suatu nilai 
+ */
+public class PBO310117095Latihan18Kelipatan {
+    
+    public static void main(String[] args) {
+   
+    for (double i=3.5; i <= 35; i = i + 3.5) {
+            System.out.println(i);
+            
+    }
+    }
+    
+}
